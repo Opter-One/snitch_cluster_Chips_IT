be83383cd195f77a3da818fc54e6188e0e7dcd82
Opter-One <luca.colombo07@universitadipavia.it>
2025-10-31 14:46:08 +0100

    Removed my dot code, messy

--- sw/benchmarks/dot_product/app.mk
+++
@@ -1,6 +0,0 @@
-APP              := bench_dot
-$(APP)_BUILD_DIR := $(SN_ROOT)/sw/benchmarks/dot_product/build
-SRCS             := $(SN_ROOT)/sw/benchmarks/dot_product/src/$(APP).c
-$(APP)_INCDIRS   := $(SN_ROOT)/sw/benchmarks/dot_product/data
-
-include $(SN_ROOT)/sw/kernels/common.mk
